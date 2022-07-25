715345bc17c17c30ec43bbac8a89f2c93db2bb15
Erica Siegel <siegel.erica@gmail.com>
2022-07-25 09:35:08 -0700

    created function to randomize a password given selected choices

--- pwgenerator/password.py
+++ pwgenerator/password.py
@@ -1,25 +1,38 @@
 import string
 import random
 
-def random_number(length=10, upper=True, lower=True, numbers=True, special=True):
-    letters = string.ascii_letters
-    numbers = string.digits
-    speical_digits = string.punctuation
+# set global variables for the password characters
+lower_letters = string.ascii_lowercase
+upper_letters = string.ascii_uppercase
+numbers = string.digits
+special_char = string.punctuation
+
+def get_password(length = 0, upper = True, lower = True, numb = True, spec = True):
+    '''
     
-    if upper and lower and numbers and special:
-        characters = letters+numbers+speical_digits
-        pw = ''.join(random.choices(characters, k=length))
-        return pw
-    elif upper and lower and numbers:
-        characters = letters+numbers
-        pw = ''.join(random.choices(characters, k=length))
-        # print(pw)
-        return pw
-    elif upper and lower and numbers:
-        characters = letters+numbers
-        pw = ''.join(random.choices(characters, k=length))
-        # print(pw)
-        return pw
+    '''
+    # set an empty password string
+    pw = '' 
     
+    # check to see if uppercase letters was selected
+    if upper: 
+        pw += upper_letters
     
-random_number()
+    # check to see if lowercase letters was selected
+    if lower:
+        pw += lower_letters
+    
+    # check to see if numbers were selected
+    if numb:
+        pw += numbers
+    
+    # check to see if special characters were selected
+    if spec:
+        pw += special_char
+    
+    # take the new password then randomize the choices, and join them, with the selected length
+    pw = ''.join(random.choices(pw, k=length))
+    # print(pw)
+    return pw
+
+get_password()
